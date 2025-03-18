2deb10ddfd3d4b9b70afd826f49dc0d758be10f4
bWFuanVzYWth <origin_0110@outlook.com>
2025-03-19 01:24:11 +0800

    新增两个api：查询物品名字和id

--- src/item.rs
+++ src/item.rs
@@ -1,6 +1,9 @@
 include!(concat!(env!("OUT_DIR"), "\\dspdb_items.rs"));
 
-// pub fn items() -> ItemProtoSet {
-//     let json_str = include_str!("../data/ItemProtoSet.json");
-//     serde_json::from_str(json_str).unwrap()
-// }
+pub fn item_name(id: i16) -> Option<String> {
+    ITEMS_NAME.get(&id).map(|name| name.to_string())
+}
+
+pub fn item_id(name: &str) -> Option<i16> {
+    ITEMS_ID.get(name).copied()
+}
